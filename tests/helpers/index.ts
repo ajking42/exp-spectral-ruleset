90e9375559bd1f5ddfa2a3332c8a714ac64ff676
Emilia Jaser <emilia.jaser@exentra.de>
2023-08-23 16:42:24 +0200

    🔥 Remove unneccessary files

--- tests/helpers/index.ts
+++
@@ -1,5 +0,0 @@
-import { loadOpenApiSpec } from './fixtures';
-import { lint } from './spectral-facade';
-import { OpenApiSpec } from './types';
-
-export { lint, loadOpenApiSpec, OpenApiSpec };
--- tests/helpers/types.ts
+++
@@ -1,1 +0,0 @@
-export type OpenApiSpec = any; // eslint-disable-line @typescript-eslint/no-explicit-any
